d862eae8832234217be7950067db8d139f20a0a9
AlvessGS <gabriels.alvess25@gmail.com>
2025-01-17 21:35:07 -0300

    add rotas and models and contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './componets/footer/Footer';
 import Navbar from './componets/navbar/Navbar';
+import { AuthProvider } from './contexts/AuthContext';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login'
+import Cadastro from './pages/cadastro/Cadastro'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>  
     </>
-  );
+  )
 }
 
-export default App;
+export default App
